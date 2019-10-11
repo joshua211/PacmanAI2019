7e3555897147622f353b6f7dc60731bdc8f1bdcc
Lycosh <Lycosh211@gmx.de>
2019-10-11 12:35:00 +0200

    Praktikum Aufgabe 1.1 & 1.2. Knoten Klasse erstellt

--- src/de/fh/stud/p1/Knoten.java
+++ src/de/fh/stud/p1/Knoten.java
@@ -1,21 +1,45 @@
 package de.fh.stud.p1;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import de.fh.pacman.enums.PacmanTileType;
 
 public class Knoten {
 	
-	/*
-	 * TODO Praktikum 1 [1]: Erweitert diese Klasse um die notwendigen
-	 * Attribute, Methoden und Konstruktoren um die möglichen verschiedenen Weltzustände darstellen und
-	 * einen Suchbaum aufspannen zu können.
-	 */
+	PacmanTileType[][] view;
+	Knoten parent;
+	int x, y;
+	PacmanTileType northTile, eastTile, southTile, westTile, current;
+
+	public Knoten(PacmanTileType[][] view, int x, int y, Knoten parent ) {
+		this.view = view;
+		this.x = x;
+		this.y = y;
+		this.parent = parent;
+		this.northTile = view[x][y-1];
+		this.eastTile = view[x+1][y];
+		this.southTile = view[x][y+1];
+		this.westTile = view[x-1][y];
+		this.current = view[x][y];
+	}
 
 	public List<Knoten> expand() {
-		/*
-		 * TODO Praktikum 1 [2]: Implementiert in dieser Methode das Expandieren des Knotens.
-		 * Die Methode soll die neu erzeugten Knoten (die Kinder des Knoten) zurückgeben.
-		 */
-		return null;
+		List<Knoten> children = new ArrayList<Knoten>();
+
+		if(northTile != PacmanTileType.WALL) 
+			children.add(new Knoten(view, x, y-1, this));
+			
+		if(eastTile != PacmanTileType.WALL) 
+			children.add(new Knoten(view, x+1, y, this));
+
+		if(southTile != PacmanTileType.WALL) 
+			children.add(new Knoten(view, x, y+1, this));
+
+		if(westTile != PacmanTileType.WALL) 
+			children.add(new Knoten(view, x-1, y, this));
+
+		return children;
 	}
 	
 }
